1d26725b716f59209888140255cbaaae4fc67453
Adrian Molina <adrianmolina39@gmail.com>
2023-02-09 06:51:16 -0400

    Navbar and footer, first look to the desing

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -4,17 +4,29 @@ import svgs from '../assets/svg/svgs';
 const Navbar = () => {
   return (
     <>
-      <nav className='Navbar'>
-        <div><ul>
-          <li>Catalogo</li>
-          <li>Comprar</li>
-          <li>Vender</li>
-        </ul></div>
-        <div><h1>LOGO NAME</h1></div>
-        <div>
-          <ul>
-            <li><img className='login-svg svg' src={svgs.userLog} alt="icono de iniciar sesión" /></li>
-          </ul>
+
+      <nav className='navbar' style={{ background: '#3A9EFD' }}>
+        <div className='container-fluid d-flex justify-content-around'>
+
+          <div className='row'>
+            <div class="col mx-5 text-center">
+              <div><h3 className='fw-bold'>AUTOSUSADOS.COM</h3></div>
+            </div>
+
+            <div class="col mx-5 text-center">
+              <div>
+                <input type="text" className='rounded' style={{ width: "30vw", height: "6vh", border: '2px solid #f0f0f0' }} />
+              </div>
+            </div>
+
+            <div class="col mx-5 text-center ">
+              <div className='my-auto d-block'>
+                <h4><a>Login </a>/ <a> Register</a> </h4>
+              </div>
+            </div>
+          </div>
+          <div>
+          </div>
         </div>
       </nav>
     </>
--- Client/src/pages/Routes/Home.jsx
+++ Client/src/pages/Routes/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='Home'>
-      <h1>page Home</h1>
+      <h1 style={{margin: '130px'}}>page Home</h1>
     </div>
   );
 };
