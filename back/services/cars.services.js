77fb21492769ec284deda62acf0f683538c18871
scampo <scampo4@hotmail.com>
2023-02-09 20:25:46 -0500

    endpoint para listar vehiculos

--- back/services/cars.services.js
+++ back/services/cars.services.js
@@ -1,7 +1,21 @@
 const Cars = require("../models/cars.model");
+const Users = require("../models/users.model");
 
 
 class CarServices {
+    static async getVehicles() {
+        try {
+            const allCars = await Cars.findAll({
+                include: {
+                    model: Users,
+                    attributes: ["fristName", "lastName"]
+                }
+            });
+            return allCars;
+        } catch (error) {
+            throw error;
+        }
+    }
     static async addVehicle(newCar) {
         try {
             const result = Cars.create(newCar);
