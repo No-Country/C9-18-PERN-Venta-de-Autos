8d68ec27d6b05b35ce42d4b0d24f1b81ddf8de1a
julandrod <julian.andres.rodriguez@gmail.com>
2023-02-15 22:40:20 -0500

    <feat> refactor register user controller and services

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -1,28 +1,67 @@
-const createHttpError = require('http-errors');
-const { endpointResponse } = require('../helpers/success');
-const { catchAsync } = require('../helpers/catchAsync');
-const UserServices = require('../services/users.services');
-const { request } = require('express');
+const createHttpError = require("http-errors");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const UserServices = require("../services/users.services");
+const { request } = require("express");
+const { createJwt } = require("../helpers");
+const sendVerificationEmail = require("../utils/sendVerificationEmail");
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
   register: catchAsync(async (req, res, next) => {
-    try {
-      const body = req.body;
-      const result = await UserServices.registerUser(body);
-      endpointResponse({
-        res,
-        message: 'User register successfully',
-        body: result,
-        code: 201,
-      });
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving index] - [index - GET]: ${error.message}`
+    const body = req.body;
+
+    const { email, firstName, lastName, password } = req.body;
+    // Validacion temporal, despues se usara express validator
+    if (!email || !firstName || !lastName || !password) {
+      return next(
+        createHttpError(400, "Por favor ingrese todos los datos necesarios")
       );
-      next(httpError);
     }
+
+    const [response, created] = await UserServices.registerUser(body);
+    if (!created) {
+      return next(
+        createHttpError(400, "Este email o telefono ya esta registrado")
+      );
+    }
+
+    const origin = "http://localhost:5000";
+      await sendVerificationEmail({
+        name: response.dataValues.firstName,
+        email: response.dataValues.email,
+        verificationToken: response.dataValues.verificationToken,
+        origin,
+      });
+
+    const {verificationToken, ...userInfo} = response.dataValues
+
+    const tokenUser = createJwt({ payload: userInfo });
+    endpointResponse({
+      res,
+      code: 201,
+      message: "Usuario creado con exito",
+      body: {
+        token: tokenUser,
+      },
+    });
+
+    // try {
+    //   const body = req.body;
+    //   const result = await UserServices.registerUser(body);
+    //   endpointResponse({
+    //     res,
+    //     message: "User register successfully",
+    //     body: result,
+    //     code: 201,
+    //   });
+    // } catch (error) {
+    //   const httpError = createHttpError(
+    //     error.statusCode,
+    //     `[Error retrieving index] - [index - GET]: ${error.message}`
+    //   );
+    //   next(httpError);
+    // }
   }),
 
   getAll: catchAsync(async (req, res, next) => {
@@ -30,7 +69,7 @@ module.exports = {
       const result = await UserServices.findUsers();
       endpointResponse({
         res,
-        message: 'Find users successfully',
+        message: "Find users successfully",
         body: result,
         code: 200,
       });
@@ -49,7 +88,7 @@ module.exports = {
       const result = await UserServices.findUserById(id);
       endpointResponse({
         res,
-        message: 'Find user by Id successfully',
+        message: "Find user by Id successfully",
         body: result,
         code: 200,
       });
@@ -69,7 +108,7 @@ module.exports = {
       const result = await UserServices.updateUser(id, body);
       endpointResponse({
         res,
-        message: 'Update user successfully',
+        message: "Update user successfully",
         body: result,
         code: 200,
       });
@@ -88,7 +127,7 @@ module.exports = {
       const result = await UserServices.deleteUser(id);
       endpointResponse({
         res,
-        message: 'Delete user successfully',
+        message: "Delete user successfully",
         body: result,
         code: 200,
       });
@@ -101,15 +140,25 @@ module.exports = {
     }
   }),
 
-  verifyUser: catchAsync(async(req, res, next)=>{
-    try{
+  verifyUser: catchAsync(async (req, res, next) => {
+    const { token, email } = req.query;
 
-    }catch(error){
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving index] - [index - GET]: ${error.message}`
-      );
-      next(httpError);
-    }
-  })
+    const user = await UserServices.findUserByEmail(email);
+
+    if (user.verificationToken !== token)
+      throw new Error("el token no es correcto");
+
+    user.verified = true;
+
+    await user.save();
+
+    const { password: pass, verificationToken, ...userInfo } = user.dataValues;
+
+    endpointResponse({
+      res,
+      message: "Usuario verificado con exito",
+      body: userInfo,
+      code: 200,
+    });
+  }),
 };
