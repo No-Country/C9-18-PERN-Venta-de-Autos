f51dcd4090db6f4429f7944f615d77e1edd18d6f
julandrod <julian.andres.rodriguez@gmail.com>
2023-02-24 11:15:19 -0500

    <add> messages seeder

--- back/seeders/seeders.js
+++ back/seeders/seeders.js
@@ -6,17 +6,24 @@ const userSeed = require("./users.seed");
 const carSeed = require("./cars.seed");
 const reviewsSeed = require("./reviews.seed");
 const Reviews = require("../models/reviews.model");
+const messagesSeed = require("./messages.seed");
+const Messages = require("../models/messages.model");
 
 initModels();
 
-let users, sellerIds, cars, carIds, reviews;
-userSeed().then((data) => {
-  users = data;
-  sellerIds = users.map((user) => user.id);
-  cars = carSeed(sellerIds);
-  carIds = cars.map((car) => car.id);
-  reviews = reviewsSeed(sellerIds, carIds);
-});
+let users, sellerIds, cars, carIds, reviews, messages;
+userSeed()
+  .then((data) => {
+    users = data;
+    sellerIds = users.map((user) => user.id);
+    cars = carSeed(sellerIds);
+    carIds = cars.map((car) => car.id);
+    reviews = reviewsSeed(sellerIds, carIds);
+    messages = messagesSeed(sellerIds, carIds);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 db.sync({ force: true }).then(() => {
   console.log("Seeding...");
@@ -25,5 +32,6 @@ db.sync({ force: true }).then(() => {
   setTimeout(() => {
     users.forEach(async (user) => await Users.create(user));
     reviews.forEach(async (review) => await Reviews.create(review));
+    messages.forEach(async (message) => await Messages.create(message));
   }, 100);
 });
